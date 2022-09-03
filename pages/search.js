00d7b2bd5151e769f27dceb4aa8e627c71df2ef4
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-03 20:07:46 +1200

    bug: API fetch please work :(

--- pages/search.js
+++ pages/search.js
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import Response from "../Response";
 import {SearchHeader} from "../components/SearchHeader";
 import {useRouter} from "next/router";
 import {SearchResults} from "../components/SearchResults";
@@ -29,10 +30,13 @@ export async function getServerSideProps(context) {
     const mockData = false;
     const data = mockData
         ? Response
-        : await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
-        &cx=${process.env.CONTEXT_KEY}
-        &q=${context.query.term}${context.query.searchType && "&searchType=image"}
-        &start=${startIndex}`).then((response) => response.json());
+        : await fetch(
+            `https://www.googleapis.com/customsearch/v1?key=${
+                process.env.API_KEY
+            }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
+                context.query.searchType && "&searchType=image"
+            }&start=${startIndex}`
+        ).then((response) => response.json());
     return {
         props: {
             results: data,

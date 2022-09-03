24693cf1b1e3e581d36e9393f418d83c9344ea31
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-03 20:00:30 +1200

    bug: API fetch fixed?

--- pages/search.js
+++ pages/search.js
@@ -1,11 +1,10 @@
 import Head from "next/head";
 import {SearchHeader} from "../components/SearchHeader";
 import {useRouter} from "next/router";
-import Response from "../Response";
 import {SearchResults} from "../components/SearchResults";
 import {ImageResults} from "../components/ImageResults";
 
-export default function Search({ results }) {
+export default function Search({results}) {
     console.log(results);
     const router = useRouter();
 
@@ -27,16 +26,10 @@ export default function Search({ results }) {
 
 export async function getServerSideProps(context) {
     const startIndex = context.query.start || "1";
-    const mockData = false;
-    const data = mockData
-        ? Response
-        : await fetch(
-            `https://www.googleapis.com/customsearch/v1?key=${
-                process.env.API_KEY
-            }&cx=${process.env.CONTEXT_KEY}&q=${context.query.term}${
-                context.query.searchType && "&searchType=image"
-            }&start=${startIndex}`
-        ).then((response) => response.json());
+    await fetch(`https://www.googleapis.com/customsearch/v1?key=${process.env.API_KEY}
+    &cx=${process.env.CONTEXT_KEY}
+    &q=${context.query.term}${context.query.searchType && "&searchType=image"}
+    &start=${startIndex}`).then((response) => response.json());
     return {
         props: {
             results: data,

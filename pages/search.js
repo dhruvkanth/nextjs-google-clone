22f06a37438c5110444c52d8d71c398907e21bbd
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-03 13:01:37 +1200

    feat: Gets the search data from Google API

--- pages/search.js
+++ pages/search.js
@@ -1,8 +1,10 @@
 import Head from "next/head";
 import {SearchHeader} from "../components/SearchHeader";
 import {useRouter} from "next/router";
+import Response from "../Response";
 
-const search = () => {
+const search = ({results}) => {
+    console.log(results);
     const router = useRouter();
 
     return (
@@ -14,6 +16,27 @@ const search = () => {
             <SearchHeader/>
         </div>
     )
+}
+
+export async function getServerSideProps(context) {
+    const startIndex = context.query.start || "1";
+    const mockData = true;
+    const data = mockData
+        ? Response
+        : await fetch(
+            `https://www.googleapis.com/customsearch/v1?key=${
+                process.env.API_KEY
+            }&cx=${
+                process.env.CONTEXT_KEY
+            }&q=${
+                context.query.term
+            }${context.query.searchType && "&searchType=image"}&start=${startIndex}`
+        ).then((response) => response.json());
+    return {
+        props: {
+            results: data,
+        },
+    };
 }
 
 export default search;

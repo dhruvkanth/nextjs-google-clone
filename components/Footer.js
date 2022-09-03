353971c606960249edd34b2cfa78f0278b85edfa
dhruvkanth <100739087+dhruvkanth@users.noreply.github.com>
2022-09-03 20:38:47 +1200

    feat: Modified the footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,8 @@
 export const Footer = () => {
     return (
-        <footer className="absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
-            <p>Copyright &copy; {new Date().getFullYear()} | This website is created for learning purposes</p>
+        <footer className="flex flex-col items-center absolute bottom-0 left-[50%] translate-x-[-50%] whitespace-nowrap p-6 text-sm text-gray-600">
+            <p>Copyright &copy; {new Date().getFullYear()} | Dhruvkanth Puli</p>
+            <p>This website is created for learning purposes</p>
         </footer>
     )
 }
